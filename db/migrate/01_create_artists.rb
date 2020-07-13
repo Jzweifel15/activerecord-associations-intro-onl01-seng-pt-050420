8e30767b3d3f5ee75ed6e763471a094e38ecc6bd
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-07-13 17:21:51 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,7 +1,7 @@
 class CreateArtists < ActiveRecord::Migration[4.2]
   
   def change
-    create_table :artist do |column|
+    create_table :artists do |column|
       column.string :name
     end
   end
